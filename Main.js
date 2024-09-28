122605e02e4f5a3930cc86f72711a485bb75b3de
ZixUUUs <[200~161074193+ZixUUUs@users.noreply.github.com>
2024-09-28 20:31:07 +0200

    getting the logic // operators and equal btn

--- Main.js
+++ Main.js
@@ -1,38 +1,50 @@
-let a = document.querySelector(".number");
-let b = document.querySelector(".number");
-let operator = document.querySelector("operator");
 const btns = document.querySelectorAll(".number");
 const input = document.querySelector("input");
 
+let firstNum = null;
+let operator = null;
+
 btns.forEach((bt) => {
   bt.addEventListener("click", (e) => {
-    input.value += e.target.innerHTML;
+    input.value += e.target.value;
   });
 });
 
-function add() {
+function onOperatorClick(op) {
+  firstNum = parseInt(input.value);
+  operator = op;
+  input.value = "";
+}
+
+function add(a, b) {
   return a + b;
 }
-function sub() {
+function sub(a, b) {
   return a - b;
 }
-function multiply() {
+function multiply(a, b) {
   return a * b;
 }
-function devide() {
-  return a / b;
+function devide(a, b) {
+  if (b == 0) {
+    alert("you dump!!!!!!! mareg nta!?");
+    return null;
+  } else {
+    return a / b;
+  }
 }
-function operate(a, b, operator) {
-  if ((operator = add())) {
-    return add(a, b);
+
+function operate() {
+  let secondNum = parseInt(input.value);
+  let result;
+  if (operator === "+") {
+    result = add(firstNum, secondNum);
+  } else if (operator === "-") {
+    result = sub(firstNum, secondNum);
+  } else if (operator === "x") {
+    result = multiply(firstNum, secondNum);
+  } else if (operator === "/") {
+    result = devide(firstNum, secondNum);
   }
-  if ((operator = sub())) {
-    return sub(a, b);
-  }
-  if ((operator = multiply())) {
-    return multiply(a, b);
-  }
-  if ((operator = devide())) {
-    return devide(a, b);
-  }
+  input.value = result;
 }
